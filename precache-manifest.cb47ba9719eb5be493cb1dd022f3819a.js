2de8f5a54855b8c66d646aad44737c3a580f7acc
tbrew1023 <tbrew1023@gmail.com>
2019-06-12 19:55:54 -0500

    Updates

---
+++ precache-manifest.cb47ba9719eb5be493cb1dd022f3819a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40da32d287139ada290bce682027445b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "cf7ecf128000499d50b9",
+    "url": "/todo-app/static/css/main.f4bacd62.chunk.css"
+  },
+  {
+    "revision": "571e99f7ebcd045646b8",
+    "url": "/todo-app/static/js/2.99b959c1.chunk.js"
+  },
+  {
+    "revision": "cf7ecf128000499d50b9",
+    "url": "/todo-app/static/js/main.4edab819.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
